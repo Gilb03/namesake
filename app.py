aad6a749b9cd9494edb03c89a0791135dd10db7f
Gilbert King <gilbertking91@gmail.com>
2020-04-30 17:20:51 -0500

    cleanup

---
+++ app.py
@@ -0,0 +1,21 @@
+print("How many kilometers did you drive today?")
+kms = input()
+miles = float(kms)/1.60934
+miles = round(miles, 2)
+print(f"Your {kms} km ride is equal to {miles} miles")
+
+
+print("How many miless did you drive today?")
+kms = input()
+miles = float(kms)/1.60934
+miles = round(miles, 2)
+print(f"Your {kms} km ride is equal to {miles} miles")
+
+
+
+print("How many kilometers did you drive today?")
+kms = input()
+miles = float(kms)/1.60934
+miles = round(miles, 2)
+print(f"Your {kms} km ride is equal to {miles} miles")
+
--- miles.py
+++
@@ -1,6 +0,0 @@
-print("How many kilometers did you drive today?")
-kms = input()
-miles = float(kms)/1.60934
-miles = round(miles, 2)
-print(f"Your {kms} km ride is equal to {miles} miles")
-
